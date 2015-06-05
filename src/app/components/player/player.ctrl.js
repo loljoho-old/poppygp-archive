63be572c07a2661ebaa4bd9550cbff586d90a95f
loljoho <jho1290@smail.raritanval.edu>
2015-06-05 00:28:16 -0400

    WIP - Layout

--- src/app/components/player/player.ctrl.js
+++ src/app/components/player/player.ctrl.js
@@ -2,20 +2,10 @@
 'use strict';
 
 angular.module('poppyGP.components')
-  .controller('PlayerCtrl', PlayerCtrl)
-  .filter('trustAsResourceUrl', trustAsResourceUrl);
+  .controller('PlayerCtrl', PlayerCtrl);
 
 /* @ngInject */
-function PlayerCtrl(urlService) {
-  var self = this;
-
-  self.url = setUrl;
-
-  function setUrl() {
-    urlService.video().$promise.then(function(response) {
-      return response;
-    });
-  }
+function PlayerCtrl() {
 
 }
 
