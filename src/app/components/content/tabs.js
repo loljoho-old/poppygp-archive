d9fd0c1f3cda27e8e75f5dae0b78481988ddbd20
loljoho <jho1290@smail.raritanval.edu>
2015-07-16 19:47:54 -0400

    Restructuring application

--- src/app/components/content/tabs.js
+++ src/app/components/content/tabs.js
@@ -1,48 +1,48 @@
 (function() {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('poppyGP.content')
-        .controller('Tabs', Tabs);
+  angular
+    .module('poppyGP.content')
+    .controller('Tabs', Tabs);
 
-    Tabs.$inject = ['$log', '$q'];
-    function Tabs($log, $q) {
-        var vm = this;
-        vm.tabs = [];
+  Tabs.$inject = ['$log', '$q'];
+  function Tabs($log, $q) {
+    var vm = this;
+    vm.tabs = [];
 
-        activate();
+    activate();
 
-        ////////////
+    ////////////
 
-        function activate() {
-            vm.tabs = [
-                {
-                    title: 'Home',
-                    icon: './assets/icons/home.svg',
-                    templateUrl: './assets/partials/tabs/home.html'
-                },
-                {
-                    title: 'Chat',
-                    icon: './assets/icons/webchat.svg',
-                    templateUrl: './assets/partials/tabs/chat.html'
-                },
-                {
-                    title: 'Help',
-                    icon: './assets/icons/help.svg',
-                    templateUrl: './assets/partials/tabs/help.html'
-                },
-                {
-                    title: 'Contact',
-                    icon: './assets/icons/contact.svg',
-                    templateUrl: './assets/partials/tabs/contact.html'
-                },
-                {
-                    title: 'About',
-                    icon: './assets/icons/about.svg',
-                    templateUrl: './assets/partials/tabs/about.html'
-                }
-            ];
-            return vm.tabs;
+    function activate() {
+      vm.tabs = [
+        {
+          title: 'Home',
+          icon: './assets/icons/home.svg',
+          templateUrl: './assets/partials/tabs/home.html'
+        },
+        {
+          title: 'Chat',
+          icon: './assets/icons/webchat.svg',
+          templateUrl: './assets/partials/tabs/chat.html'
+        },
+        {
+          title: 'Help',
+          icon: './assets/icons/help.svg',
+          templateUrl: './assets/partials/tabs/help.html'
+        },
+        {
+          title: 'Contact',
+          icon: './assets/icons/contact.svg',
+          templateUrl: './assets/partials/tabs/contact.html'
+        },
+        {
+          title: 'About',
+          icon: './assets/icons/about.svg',
+          templateUrl: './assets/partials/tabs/about.html'
         }
+      ];
+      return vm.tabs;
     }
+  }
 })();
---
+++ src/app/core/view/view.controller.js
@@ -0,0 +1,28 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('poppyGP.core')
+    .controller('View', View);
+
+  // @ngInject
+  function View($mdSidenav, $mdMedia, dataservice) {
+    var vm = this;
+    vm.title = '';
+
+
+    activate();
+
+    ////////////
+
+    function activate() {
+      return dataservice.current().then(function(response) {
+          vm.title = response.year + ' ' + response.title;
+          return vm.title;
+        }, function(errorMsg) {
+          vm.title = errorMsg;
+          return vm.title;
+        });
+    }
+  }
+})();
