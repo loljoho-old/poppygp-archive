33f2eb23ddbea990848eac9d117f477e6563e2ba
loljoho <jho1290@smail.raritanval.edu>
2015-07-28 05:51:49 -0400

    Moved Material Icon svgs

--- src/app/core/scripts/icon.config.js
+++ src/app/core/scripts/icon.config.js
@@ -21,17 +21,17 @@
 
       $mdIconProvider
 
-        .icon('close',            './assets/svg/ic_close_white_24px.svg')
-        .icon('arrow-left',       './assets/svg/ic_arrow_back_white_24px.svg')
-        .icon('arrow-right',      './assets/svg/ic_arrow_forward_white_24px.svg')
-        .icon('menu-bars',        './assets/svg/ic_menu_white_24px.svg')
+        .icon('close',            './assets/svg/material/ic_close_white_24px.svg')
+        .icon('arrow-left',       './assets/svg/material/ic_arrow_back_white_24px.svg')
+        .icon('arrow-right',      './assets/svg/material/ic_arrow_forward_white_24px.svg')
+        .icon('menu-bars',        './assets/svg/material/ic_menu_white_24px.svg')
 
-        .icon('chevron-up',       './assets/svg/ic_expand_less_black_24px.svg')
-        .icon('chevron-down',     './assets/svg/ic_expand_more_black_24px.svg')
-        .icon('fullscreen',       './assets/svg/ic_fullscreen_black_24px.svg')
-        .icon('fullscreen-exit',  './assets/svg/ic_fullscreen_exit_black_24px.svg')
-        .icon('more-vert',        './assets/svg/ic_more_vert_black_24px.svg')
-        .icon('more-horiz',       './assets/svg/ic_more_horiz_black_24px.svg');
+        .icon('chevron-up',       './assets/svg/material/ic_expand_less_black_24px.svg')
+        .icon('chevron-down',     './assets/svg/material/ic_expand_more_black_24px.svg')
+        .icon('fullscreen',       './assets/svg/material/ic_fullscreen_black_24px.svg')
+        .icon('fullscreen-exit',  './assets/svg/material/ic_fullscreen_exit_black_24px.svg')
+        .icon('more-vert',        './assets/svg/material/ic_more_vert_black_24px.svg')
+        .icon('more-horiz',       './assets/svg/material/ic_more_horiz_black_24px.svg');
     }
 
 
